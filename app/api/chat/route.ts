1def5038efa9b0648f90f90ba9c9938883fa5f42
rchrisarevalo <rubenplayer105@gmail.com>
2024-08-06 08:44:35 -0500

    Included template from Bill's video to start off project

---
+++ app/api/chat/route.ts
@@ -0,0 +1,6 @@
+import { NextResponse } from "next/server";
+
+// Template included from Bill's video.
+const POST = (res: NextResponse) => {
+    return NextResponse.json({ message: "This is an api endpoint "})
+}
