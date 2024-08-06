1f0758bcace1028b80a549d494a9449c34f9ff10
rchrisarevalo <rubenplayer105@gmail.com>
2024-08-06 16:48:14 -0500

    Basic API setup and UI enhancements made for mobile version

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,8 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 
 // Template included from Bill's video.
-const GET = async (req: NextRequest) => {
-    return NextResponse.json({ message: "This is an API endpoint."})
+const POST = async (req: NextRequest) => {
+    const data = await req.json()
+
+    return NextResponse.json({ message: data.chat_prompt })
 }
 
-export { GET }
+export { POST }
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,48 @@
+'use client';
+import React, { useState } from "react";
+
 export default function Home() {
+  const [chatInput, setChatInput] = useState<string>("")
+
+  const handleChatSubmission = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    try {
+      const res = await fetch("/api/chat", {
+        method: 'POST',
+        body: JSON.stringify({
+          chat_prompt: chatInput
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+
+      if (res.ok) {
+        const data = await res.json()
+
+        console.log(data)
+      }
+    } catch {
+      throw new Error("Failed to submit chat.")
+    }
+  };
+
   return (
     <main className="flex min-h-screen bg-white text-black flex-col items-center justify-center">
-      <div className="flex flex-col item-center bg-slate-100 justify-center gap-5 text-left ml-32 mr-32 p-20 rounded-2xl">
-        <h1 className="font-extrabold text-3xl">Cutomer Support Chatbot</h1>
-        <i>
-          Ask any questions you might have. and we will give them to you!
-        </i>
-        <form className="flex flex-row items-center justify-center gap-10 text-left rounded-md">
-          <textarea className="border-transparent pb-4 bg-white outline-transparent p-4 items-center resize-none w-full" required></textarea>
+      <div className="flex flex-col item-center bg-slate-200 justify-center gap-5 text-left ml-32 mr-32 max-sm:ml-4 max-sm:mr-4 p-32 max-sm:p-12 rounded-2xl">
+        <h1 className="font-extrabold text-3xl max-sm:text-2xl">Customer Support Chatbot</h1>
+        <i>Ask any questions you might have, and we will give the answers to you!</i>
+        <form onSubmit={handleChatSubmission} className="flex flex-row max-sm:flex-col items-center justify-center gap-10 text-left rounded-md">
+          <input
+            className="border-transparent pb-4 bg-white outline-transparent p-4 items-center resize-none w-full"
+            onChange={(e) => setChatInput(e.target.value)}
+            required
+          />
           <div className="flex flex-col justify-center items-center">
-            <button className="p-4 py-2 rounded-md bg-slate-500 text-white font-extrabold">Send</button>
+            <button type="submit" className="p-4 py-2 rounded-md bg-slate-500 text-white font-extrabold">
+              Send
+            </button>
           </div>
         </form>
       </div>
