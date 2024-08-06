fd8616383ba1bf17fc3e63a40a102fe1a747e8f6
rchrisarevalo <rubenplayer105@gmail.com>
2024-08-06 13:44:56 -0500

    Turned API function to using GET HTTP protocol

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,6 +1,8 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 // Template included from Bill's video.
-const POST = (res: NextResponse) => {
-    return NextResponse.json({ message: "This is an api endpoint "})
+const GET = async (req: NextRequest) => {
+    return NextResponse.json({ message: "This is an API endpoint."})
 }
+
+export { GET }
