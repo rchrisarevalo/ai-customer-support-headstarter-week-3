c50cc1ea4654a2f8ec0828da8383e2061ce04ebf
rchrisarevalo <rubenplayer105@gmail.com>
2024-08-08 13:11:00 -0500

    Updated dashboard UI

--- app/components/Dashboard.tsx
+++ app/components/Dashboard.tsx
@@ -16,7 +16,7 @@ export const ChatBotMessage: React.FC<
   MessageDisplayProps & { isLoading: boolean }
 > = ({ message, isLoading }) => {
   return (
-    <div className="flex flex-row max-sm:flex-col gap-5 items-left text-left">
+    <div className="flex flex-col gap-5 items-start text-left">
       <div className="flex items-center">
         <Image
           src={chatbot_img}
@@ -29,7 +29,7 @@ export const ChatBotMessage: React.FC<
           <Loading isLoading={isLoading} />
         </div>
       ) : (
-        <div className="p-5 w-3/5 max-sm:w-3/4 bg-blue-500 text-white rounded-md">
+        <div className="p-5 w-3/5 max-sm:w-3/4 bg-slate-300 text-black rounded-md">
           <p className="mr-10 max-sm:mr-0">
             {message.split("\n").map((par, i) => (
               <span key={`par-${i}`} className="flex flex-col gap-4">{par}</span>
@@ -43,27 +43,20 @@ export const ChatBotMessage: React.FC<
 
 export const UserMessage: React.FC<MessageDisplayProps> = ({ message }) => {
   return (
-    <div className="flex flex-row justify-end items-center max-sm:flex-col gap-5 text-left">
-      <div className="sm:hidden flex items-center">
+    <div className="flex flex-col items-end max-sm:items-end gap-5 text-left">
+      <div className="flex flex-row items-center">
         <Image
           src={user_img}
           alt="user-ai-generated"
           className="w-16 h-16 rounded-full pointer-events-none"
         />
       </div>
-      <div className="p-5 w-3/5 max-sm:w-3/4 bg-slate-300 text-black rounded-md">
+      <div className="p-5 w-3/5 max-sm:w-3/4 bg-blue-500 text-white rounded-md">
         <p className="mr-10 max-sm:mr-0">
           {message.split("\n").map((par, i) => (
             <span key={`par-${i}`} className="flex flex-col gap-4">{par}</span>
           ))}
         </p>
-      </div>
-      <div className="max-sm:hidden flex flex-row items-center">
-        <Image
-          src={user_img}
-          alt="user-ai-generated"
-          className="w-16 h-16 rounded-full pointer-events-none"
-        />
       </div>
     </div>
   );
@@ -71,7 +64,7 @@ export const UserMessage: React.FC<MessageDisplayProps> = ({ message }) => {
 
 export const MessageDashboard: React.FC<DashboardProps> = ({ children }) => {
   return (
-    <div className="flex flex-col p-10 max-sm:p-5 gap-5 max-lg:ml-10 max-lg:mr-10 max-sm:ml-0 max-sm:mr-0 max-sm:py-5 rounded-md max-sm:w-full bg-white text-black">
+    <div className="flex flex-col min-w-full h-96 overflow-y-scroll p-10 max-sm:p-5 gap-5 max-lg:ml-10 max-lg:mr-10 max-sm:ml-0 max-sm:mr-0 max-sm:py-5 rounded-md max-sm:w-full bg-white text-black">
       {children}
     </div>
   );
