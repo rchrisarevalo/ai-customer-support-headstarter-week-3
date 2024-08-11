ae7556eed50362a35e1122ca76feaa95ba216d68
rchrisarevalo <rubenplayer105@gmail.com>
2024-08-11 14:32:10 -0500

    Set up API key configuration for Firestore DB

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,16 @@ const nextConfig = {
     reactStrictMode: true,
     env: {
         GROQ_API_KEY: process.env.GROQ_API_KEY,
-        GROQ_PROMPT: process.env.GROQ_PROMPT
+        GROQ_PROMPT: process.env.GROQ_PROMPT,
+        GROQ_API_KEY: process.env.GROQ_API_KEY,
+        GROQ_PROMPT: process.env.GROQ_PROMPT,
+        FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+        FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+        FIREBASE_PROJECT_ID: process.env.FIREBASE_AUTH_PROJECT_ID,
+        FIREBASE_STORAGE_BUKCET: process.env.FIREBASE_STORAGE_BUCKET,
+        FIREBASE_MSG_SENDER_ID: process.env.FIREBASE_MSG_SENDER_ID,
+        FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+        FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID
     }
 };
 
